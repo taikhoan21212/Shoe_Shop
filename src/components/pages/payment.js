80a37c2aa40ed961303bb700978b85518c8c75ca
Phan Tài <phantai3202@gmail.com>
2023-06-15 10:32:35 +0700

    new

---
+++ src/components/pages/payment.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function payMent(){
+    return(
+        <div className="main">
+            
+        </div>
+    )
+}
---
+++ src/components/pages/product.js
@@ -0,0 +1,127 @@
+import React from 'react'
+
+export const productDetails = () => {
+  return (
+    <div className='main'>
+        <div class="content">
+            <div class="content_product">
+                <div class="content_product-hot">
+                    <div class="content_product-hot-title">
+                        <h3>Sản phẩm nữ giới</h3>
+                    </div>
+                    <div class="content-product-hot-list row">
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core Hmd000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6 col-lg-3 mt-10">
+                            <img src="./img/product1.webp" alt="" class="product-img" />
+                            <div class="place-body">
+                                <h3 class="place-heading">Giày Biti's hunter</h3>
+                                <p class="place-time">Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)6</p>
+                                <p class="place-desc">700.000 đ</p>
+                                <button class="btn-product s-col-full js-buy-ticket">MUA NGAY</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
