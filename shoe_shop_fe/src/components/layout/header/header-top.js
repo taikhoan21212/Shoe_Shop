89fbf8d18ac98a9a7a307a42cd177f5964dcc12d
Zoe Wen <78011238+kimngoc998@users.noreply.github.com>
2023-07-03 01:04:50 +0700

    a

--- shoe_shop_fe/src/components/layout/header/header-top.js
+++ shoe_shop_fe/src/components/layout/header/header-top.js
@@ -2,9 +2,10 @@ import './header.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEarthAmericas, faChevronDown, faMobile, faHeart} from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from "react-redux";
 
-
-function header_top(){
+function Header_top(){
+    const user = useSelector((state)=> state.auth.login.currentUser);
     
     return(
         <div>
@@ -28,9 +29,16 @@ function header_top(){
                         <FontAwesomeIcon icon={faHeart} />
                         Yêu thích
                     </div>
+                    {user? (
+                        <>
+                        <p className="header_top-hello">Hi, <span> {user.username}  </span> </p>
+                        { user.isAdmin? (<> <Link to="/PageAdmin" className="header_top-admin"> <span> Admin page </span> </Link> </>):(<></>)}
+                                    <Link to="/logout" className="navbar-logout" > Log out</Link>
+        
+        </>
+      ) : (<>
                     <div className="header_top-register">Đăng ký</div>
-                    <div className="header_top-login"><Link to={"/Login"}>Đăng nhập</Link></div>
-                    
+                    <div className="header_top-login"><Link to={"/Login"}>Đăng nhập</Link></div></>)}
                 </div>
             </div>
             <div className="center">
@@ -40,4 +48,4 @@ function header_top(){
     );
 }
 
-export default header_top;
+export default Header_top;
