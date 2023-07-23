633fcae8e8b1e790b5bbc9a974d9a0bfd84e7a51
Zoe Wen <78011238+kimngoc998@users.noreply.github.com>
2023-07-24 04:28:54 +0700

    .

--- shoe_shop_fe/src/App.js
+++ shoe_shop_fe/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import { BrowserRouter , Routes, Route} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './components/homepage';
@@ -19,6 +19,9 @@ function App() {
 
     const [cartItems, setCartItems] = useState(cartFromLocalStorage);
 
+    useEffect(() => {
+        localStorage.setItem("cartItems",JSON.stringify(cartItems));
+    },[cartItems]);
 
     return (
         <BrowserRouter>
