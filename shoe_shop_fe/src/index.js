4ebbaa94c8d006985fba02d00a00f2ed86d73a06
Zoe Wen <78011238+kimngoc998@users.noreply.github.com>
2023-09-23 17:00:58 +0700

    eslint update

--- shoe_shop_fe/src/index.js
+++ shoe_shop_fe/src/index.js
@@ -7,6 +7,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client';
+import _ from 'lodash';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
