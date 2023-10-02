842be05ca2089021e8c44302607306ee1b1e00ff
Phan Tài <phantai3202@gmail.com>
2023-10-02 10:47:49 +0700

    s

--- shoe_shop_fe_mobile/app.config.js
+++ shoe_shop_fe_mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
   // All values in extra will be passed to your app.
   extra: {
-    // apiURL: "http://localhost:8000/v1/",
-    apiURL: "http://192.168.1.23:8000/v1/",
+    //apiURL: "http://localhost:8000/v1/",
+    apiURL: "http://192.168.1.10:8000/v1/",
   },
 };
